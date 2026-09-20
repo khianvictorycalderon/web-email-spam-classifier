4154dfc3ef8870969cb87bb7e3fcc07ba805db86
Khian Victory <ckhianvictory@gmail.com>
2026-09-20 20:57:11 +0800

    Added 2 check buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,6 +21,32 @@ export default function App() {
           <li>ASP.NET Core Web API (Backend)</li>
           <li>Flask + Tensorflow (AI Service)</li>
         </ul>
+        <hr className="my-4" />
+
+        <div
+          className="flex flex-col lg:flex-row gap-2"
+        >
+          <button
+            className="
+              cursor-pointer
+              px-12 py-2 rounded-md
+              bg-blue-600 text-white hover:bg-blue-500
+              trasition duration-300
+            "
+          >Check Server</button>
+
+          <button
+            className="
+              cursor-pointer
+              px-12 py-2 rounded-md
+              bg-green-600 text-white hover:bg-green-500
+              trasition duration-300
+            "
+          >Check AI Service</button>
+        </div>
+
+        <hr className="my-4" />
+
       </div>
       
     </main>
