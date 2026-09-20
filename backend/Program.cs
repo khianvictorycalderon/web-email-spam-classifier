538524831a02054fc3dc47e93c9adefeecace1db
Khian Victory <ckhianvictory@gmail.com>
2026-09-20 22:00:51 +0800

    Added backend CORS extension

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,9 +1,18 @@
+using backend.Extensions;
+
+// Building the API
 var builder = WebApplication.CreateBuilder(args);
 
+// Adding services
+builder.Services.AddDefaultCorsPolicy(builder.Configuration);
 builder.Services.AddControllers();
 
+// Application
 var app = builder.Build();
 
+// Middlewares
+app.UseCorsPolicy();
 app.MapControllers();
 
+// Running the main application
 app.Run();
